6f09160217d5fe432153bbdabf7cc5c6ab8b620d
Hsiajay <631853070@qq.com>
2019-07-07 18:21:02 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import lodash from 'lodash'
+
+import ElementUI from 'element-ui'
+
+import {formatTime} from "./utils/time";
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Object.defineProperty(Vue.prototype, '$_', { value: lodash })
+
+/*Vue.directive('title',  function (el, binding) {
+  document.title = el.dataset.title
+})*/
+
+// 格式化时间
+Vue.filter('format', formatTime)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
