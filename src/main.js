df9d3a3bab55a7404361ae80733a8802e074be47
Hsia2018 <631853070@qq.com>
2019-07-09 11:42:43 +0800

    lenovo commit

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 
 import lodash from 'lodash'
 
 import ElementUI from 'element-ui'
+import '@/assets/theme/index.css'
+import '@/assets/icon/iconfont.css'
 
 import {formatTime} from "./utils/time";
 
@@ -14,9 +17,9 @@ Vue.config.productionTip = false
 Vue.use(ElementUI)
 Object.defineProperty(Vue.prototype, '$_', { value: lodash })
 
-/*Vue.directive('title',  function (el, binding) {
+Vue.directive('title',  function (el, binding) {
   document.title = el.dataset.title
-})*/
+})
 
 // 格式化时间
 Vue.filter('format', formatTime)
@@ -25,6 +28,7 @@ Vue.filter('format', formatTime)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
